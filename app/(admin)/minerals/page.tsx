b60e3ef5405ef37d7c8bfe6d211d54677c58c1bc
Cedric Franz M Candido <cedricfranzmcandido@Cedrics-Mac-mini.local>
2024-08-21 16:06:20 +0800

    add ui for admin module

--- app/(admin)/minerals/page.tsx
+++ app/(admin)/minerals/page.tsx
@@ -85,45 +85,43 @@ const invoices = [
   },
 ];
 
-export default function DepartmentPage() {
+export default function MineralsPage() {
   const [position, setPosition] = useState("bottom");
 
   return (
     <div className=" flex  w-full h-screen items-center justify-center px-10 flex-col">
       <div className=" w-full items-center   flex  justify-between p-10 pl-4 mb-20">
         <p className="  text-teal-700  font-bold shadow-sm drop-shadow-sm  underline-offset-1 underline text-3xl">
-          Department
+          Minerals
         </p>
         <Popover>
           <PopoverTrigger asChild>
-            <Button className=" bg-gray-700">Add Department</Button>
+            <Button className=" bg-gray-700">Add Minerals</Button>
           </PopoverTrigger>
           <PopoverContent className="w-96">
             <div className="grid gap-4">
               <div className="space-y-2">
-                <h4 className="font-medium leading-none"> Add Department</h4>
+                <h4 className="font-medium leading-none"> Add Minerals</h4>
               </div>
               <div className="grid gap-2">
                 <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="width"> Name</Label>
+                  <Label htmlFor="width"> name:</Label>
                   <Input id="width" className="col-span-2 h-8" />
                 </div>
                 <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="maxWidth">Description</Label>
-                  <Textarea
-                    id="maxWidth"
-                    className="col-span-2    h-16  overflow-scroll"
-                    placeholder=" description of deparment"
-                  />
+                  <Label htmlFor="width"> brand:</Label>
+                  <Input id="width" className="col-span-2 h-8" />
                 </div>
                 <div className="grid grid-cols-3 items-center gap-4">
-                  <Label htmlFor="height">Logo url</Label>
-                  <Input
-                    id="height"
-                    placeholder=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTR3tl0kaO56KpCEWQJeNfk6pgwGKBoEVvXA&s"
-                    className="col-span-2 h-8"
-                  />
+                  <Label htmlFor="width"> description:</Label>
+                  <Input id="width" className="col-span-2 h-8" />
                 </div>
+
+                <div className="grid grid-cols-3 items-center gap-4">
+                  <Label htmlFor="width"> size :</Label>
+                  <Input id="width" className="col-span-2 h-8 " type="number" />
+                </div>
+
                 <div className=" w-full flex  justify-end p-3  bg">
                   <Button
                     id="maxHeight"
@@ -138,34 +136,47 @@ export default function DepartmentPage() {
           </PopoverContent>
         </Popover>
       </div>
-
       <div className=" w-full items-start   flex  justify-start gap-5  pt-0 pl-4  py-4">
         <Input
-          placeholder="search  users"
+          placeholder="search  minerals"
           className="  w-48 border  border-teal-700 text-teal-700 "
         />
+        <Select>
+          <SelectTrigger className="w-[180px] text-teal-700  text-xs">
+            <SelectValue placeholder="filter department" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectLabel>list of departments</SelectLabel>
+              <SelectItem value="ALL">ALL</SelectItem>
+              <SelectItem value="CCIS">CCIS</SelectItem>
+              <SelectItem value="CET">CET</SelectItem>
+              <SelectItem value="CAT">CAT</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
       </div>
       <Table>
         <TableCaption className=" mt-20">A list of Deparment.</TableCaption>
         <TableHeader>
           <TableRow>
-            <TableHead className=""> name</TableHead>
+            <TableHead className=""> Name</TableHead>
+            <TableHead>Brand</TableHead>
             <TableHead>Description</TableHead>
-            <TableHead></TableHead>
-
-            <TableHead>Action</TableHead>
+            <TableHead>Size </TableHead>
+            <TableHead>Actions</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {invoices.map((invoice) => (
             <TableRow key={invoice.invoice}>
               <TableCell className="font-medium">{invoice.invoice}</TableCell>
-              <TableCell>{invoice.paymentStatus}</TableCell>
-              <TableCell></TableCell>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
+              <TableCell className="font-medium">{invoice.invoice}</TableCell>
 
               <TableCell>
                 <div className=" flex  gap-4">
-                  <Button className=" bg-teal-700">Manage users</Button>
                   <Popover>
                     <PopoverTrigger asChild>
                       <Button className=" bg-orange-300">Edit</Button>
@@ -175,31 +186,32 @@ export default function DepartmentPage() {
                         <div className="space-y-2">
                           <h4 className="font-medium leading-none">
                             {" "}
-                            Add Department
+                            Add Minerals
                           </h4>
-                          <p className="text-sm text-muted-foreground"></p>
                         </div>
                         <div className="grid gap-2">
                           <div className="grid grid-cols-3 items-center gap-4">
-                            <Label htmlFor="width"> Name</Label>
+                            <Label htmlFor="width"> name:</Label>
                             <Input id="width" className="col-span-2 h-8" />
                           </div>
                           <div className="grid grid-cols-3 items-center gap-4">
-                            <Label htmlFor="maxWidth">Description</Label>
-                            <Textarea
-                              id="maxWidth"
-                              className="col-span-2    h-16  overflow-scroll"
-                              placeholder=" description of deparment"
-                            />
+                            <Label htmlFor="width"> brand:</Label>
+                            <Input id="width" className="col-span-2 h-8" />
                           </div>
                           <div className="grid grid-cols-3 items-center gap-4">
-                            <Label htmlFor="height">Logo url</Label>
+                            <Label htmlFor="width"> description:</Label>
+                            <Input id="width" className="col-span-2 h-8" />
+                          </div>
+
+                          <div className="grid grid-cols-3 items-center gap-4">
+                            <Label htmlFor="width"> size:</Label>
                             <Input
-                              id="height"
-                              placeholder=" https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTR3tl0kaO56KpCEWQJeNfk6pgwGKBoEVvXA&s"
-                              className="col-span-2 h-8"
+                              id="width"
+                              className="col-span-2 h-8 "
+                              type="number"
                             />
                           </div>
+
                           <div className=" w-full flex  justify-end p-3  bg">
                             <Button
                               id="maxHeight"
@@ -213,6 +225,7 @@ export default function DepartmentPage() {
                       </div>
                     </PopoverContent>
                   </Popover>
+
                   <Popover>
                     <PopoverTrigger asChild>
                       <Button className=" bg-red-400">Delete</Button>
