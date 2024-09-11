9bd7c7d7ae6b7a852ca40da5ff5a6c014d165863
leouie28 <tabiqueleouie10@gmail.com>
2024-09-11 22:55:08 +0800

    Remove trpc config and packages

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,23 +1,17 @@
-import { prisma } from "@/lib/prisma"
-import { generateToken, verifyToken } from "@/utils/jwt";
-import { matchPassword } from "@/utils/password"
 import { NextResponse } from 'next/server';
 import { cookies } from 'next/headers';
+import { authorize } from "@/lib/auth";
 export async function POST(req: Request) {
     try {
         const { email, password } = await req.json()
-        const user = await prisma.users.findUnique({
-            where: { email: email as string }
-        })
-        if (!user) {
+
+        const auth = await authorize(email, password)
+
+        if (!auth) {
             return NextResponse.json({message: "UNAUTHORIZED"}, { status: 401 });
         }
-        const isMatch = await matchPassword(password, user.password)
-        if (isMatch) {
-            const token = await generateToken(user)
-            cookies().set('serverToken', token);
-            return Response.json({message: 'success', data: { token, ...user } })
-        }
+        cookies().set('serverToken', auth.token);
+        return Response.json({message: 'success', data: { token: auth.token, ...auth.user } })
     } catch (error) {
         return Response.json(error, { status: 500 })
     }
